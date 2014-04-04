68e6d86ae12af4f44c0faa8bdb60c202ec161dd2
pgussow@gmail.com <pgussow@gmail.com@ca1c7030-5d88-0388-f4b8-df9b36323e2b>
2014-04-04 08:09:37 +0000

    Enhanced the command lines

--- trunk/bin/run.sh
+++ trunk/bin/run.sh
@@ -5,4 +5,17 @@ export JAVA_OPTS=-Xmx1024m
 ## In case you need to remotely debug CAAS itself
 # export JAVA_OPTS=$JAVA_OPTS -Xdebug -Xrunjdwp:transport=dt_socket,address=127.0.0.1:9302,server=y,suspend=y 
 
-$JAVA_EXE $JAVA_OPTS -jar ../caas.jar -c ../config/caas.conf -v run ../scripts/$1.groovy $2 $3 $4 $5 $6 $7 $8 $9
+# Find the name of the Groovy script to run
+export GROOVY_SCRIPT=$1
+shift
+
+# Save the rest of the parameters
+export REST_OF_PARAMETERS=
+
+while test ${#} -gt 0
+do
+  export REST_OF_PARAMETERS=$REST_OF_PARAMETERS $1
+  shift
+done
+
+$JAVA_EXE $JAVA_OPTS -jar ../caas.jar -c ../config/caas.conf -v run ../scripts/$GROOVY_SCRIPT.groovy $REST_OF_PARAMETERS
