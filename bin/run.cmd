db90b0cff5834e008a090749bb3c547645207fcd
pgussow <pgussow@gmail.com>
2014-04-04 08:09:37 +0000

    Enhanced the command lines

--- bin/run.cmd
+++ bin/run.cmd
@@ -5,5 +5,19 @@ set JAVA_OPTS=-Xmx1024m
 rem In case you need to remotely debug CAAS itself
 rem set JAVA_OPTS=%JAVA_OPTS% -Xdebug -Xrunjdwp:transport=dt_socket,address=127.0.0.1:9302,server=y,suspend=y 
 
-%JAVA_EXE% %JAVA_OPTS% -cp ../caas.jar;../classes org.kisst.cordys.caas.main.CaasMain -c ..\config\caas.conf -v run ..\scripts\%1.groovy %2 %3 %4 %5 %6 %7 %8 %9
+set GROOVY_SCRIPT=%1
 
+rem Throw the first parameter away and build up a variable with the rest of the parameters.
+SHIFT
+
+set REST_OF_PARAMETERS=%1
+
+:loop
+SHIFT
+if [%1]==[] goto afterloop
+set REST_OF_PARAMETERS=%REST_OF_PARAMETERS% %1
+goto loop
+
+:afterloop
+
+%JAVA_EXE% %JAVA_OPTS% -cp ../caas.jar;../classes org.kisst.cordys.caas.main.CaasMain -c ..\config\caas.conf -v run ..\scripts\%GROOVY_SCRIPT%.groovy %REST_OF_PARAMETERS%
